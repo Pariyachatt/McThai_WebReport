632b7aa3a318d1959f59e725da0fa5891eaf2926
Pariyachatt <pariyachat@ssci.co.th>
2023-02-21 12:52:32 +0700

    create_login

--- Login.py
+++
@@ -1,9 +0,0 @@
-# import streamlit as st 
-
-# with st.form(key='my_form'):
-#     st.write("Login")
-#     text_input = st.text_input(label='Enter your name')
-#     submit_button = st.form_submit_button(label='Submit')
-
-# if submit_button:
-#     st.session_state.runpag
